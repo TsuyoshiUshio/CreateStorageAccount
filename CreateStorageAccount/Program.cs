55ec54e8e7f0c25f4b1fdead3451b0e9bd0995b6
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-13 20:28:26 +0900

    Add project files.

---
+++ CreateStorageAccount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateStorageAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
