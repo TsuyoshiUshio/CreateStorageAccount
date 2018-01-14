9473d3d2086ff379252e2e177a94188c71e919ae
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-01-15 00:48:36 +0900

    Refactor the methods. for readability.

--- CreateStorageAccount/Program.cs
+++ CreateStorageAccount/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Azure.Management.ResourceManager;
 using Microsoft.Azure.Management.ResourceManager.Models;
 
 using System.Linq;
-
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
 
 namespace CreateStorageAccount
 {
@@ -33,40 +33,62 @@ namespace CreateStorageAccount
              clientId = Configuration["clientId"];
              clientSecret = Configuration["clientSecret"];
              tenantId = Configuration["tenantId"];
-    }
+        }
 
-        private async Task MainAsync()
+        private async Task CreateOrUpdateResourceGroupAsync(AzureCredentials credentials, string location)
         {
-            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
-            using (var resourceClient = new Microsoft.Azure.Management.ResourceManager.ResourceManagementClient(credentials)) {
+            using (var resourceClient = new Microsoft.Azure.Management.ResourceManager.ResourceManagementClient(credentials))
+            {
                 var parameter = new ResourceGroup();
-                parameter.Location = "NorthEurope";
+                parameter.Location = location;
                 parameter.Name = resourceGroup;
                 resourceClient.SubscriptionId = subscriptionId;
                 await resourceClient.ResourceGroups.CreateOrUpdateAsync(resourceGroup, parameter);
             }
+        }
+
+        private async Task MainAsync()
+        {
+            var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
+
+            var location = "NorthEurope";
+
+            await CreateOrUpdateResourceGroupAsync(credentials, location);
+
             using (var client = new StorageManagementClient(credentials))
             {
-                var parameter = new StorageAccountCreateParameters();
-                parameter.Location = "NorthEurope";
-                parameter.Kind = Kind.Storage; // WE can also specify StorageV2
-                parameter.Sku = new Microsoft.Azure.Management.Storage.Models.Sku(SkuName.StandardLRS, SkuTier.Standard);
-                client.SubscriptionId = subscriptionId;
 
                 var accountName = "someaacountabc2";
                 
-                var storageAccount = await client.StorageAccounts.CreateAsync(resourceGroup, accountName, parameter);
-                
-                var properties = await client.StorageAccounts.GetPropertiesAsync(resourceGroup, accountName);
-                var keys = await client.StorageAccounts.ListKeysAsync(resourceGroup, accountName);
-                var storageAccountKeys = keys.Keys;
-                var key = storageAccountKeys.First();
+                var storageAccount = await CreateStorageAccountAsync(client, accountName, location);
 
-                Console.WriteLine($"Storage Account was created. Name: {accountName} ConnectionString: {key.Value}");
-                Console.WriteLine("Press Any Key");
-                Console.ReadLine();
+                var key = await GetAccountKeyAsync(client, accountName);
+                printStorageAccountInfo(accountName, key);
             }
             
+        }
+        private void printStorageAccountInfo(string accountName, StorageAccountKey key)
+        {
+            Console.WriteLine($"Storage Account was created. Name: {accountName} ConnectionString: {key.Value}");
+            Console.WriteLine("Press Any Key");
+            Console.ReadLine();
+        }
+        private async Task<StorageAccount> CreateStorageAccountAsync(StorageManagementClient client, string accountName, string location)
+        {
+            var parameter = new StorageAccountCreateParameters();
+            parameter.Location = location;
+            parameter.Kind = Kind.Storage; // WE can also specify StorageV2
+            parameter.Sku = new Microsoft.Azure.Management.Storage.Models.Sku(SkuName.StandardLRS, SkuTier.Standard);
+            client.SubscriptionId = subscriptionId;
+            return await client.StorageAccounts.CreateAsync(resourceGroup, accountName, parameter);
+        }
+
+        private async Task<StorageAccountKey> GetAccountKeyAsync(StorageManagementClient client, string accountName)
+        {
+            var properties = await client.StorageAccounts.GetPropertiesAsync(resourceGroup, accountName);
+            var keys = await client.StorageAccounts.ListKeysAsync(resourceGroup, accountName);
+            var storageAccountKeys = keys.Keys;
+            return storageAccountKeys.First();
         }
 
         private static string resourceGroup;
